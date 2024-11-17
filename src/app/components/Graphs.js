28aaff6bc2c83303ee78c97592aa704c94edbef2
Cemi-am <mezimez.samy@gmail.com>
2024-11-17 00:24:27 -0500

    graph

--- src/app/components/Graphs.js
+++ src/app/components/Graphs.js
@@ -1,12 +1,15 @@
 import { Bar } from "react-chartjs-2";
-import { Chart as ChartJS, BarElement, Title, Tooltip, Legend } from "chart.js";
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
 
+// Register components
 ChartJS.register(
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-);
+  CategoryScale,
+  LinearScale,  
+  BarElement,   
+  Title,        
+  Tooltip,      
+  Legend
+);       
 
 export default function Graphs({ data }) {
     const priceDistribution = (data) => {
